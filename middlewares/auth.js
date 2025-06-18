3b942950d7f3af8b235bf19e9470c8993ea90482
Chris Fairbanks <Chris2Fair88@gmail.com>
2025-06-17 20:22:47 -0400

    submit Project 13 v.4

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -8,14 +8,14 @@ const auth = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    return res.status(ERROR_MESSAGES.UNAUTHORIZED.status).send(ERROR_MESSAGES.UNAUTHORIZED.message);
+    return res.status(ERROR_MESSAGES.UNAUTHORIZED.status).send({ message: ERROR_MESSAGES.UNAUTHORIZED.message });
   }
   const token = authorization.replace('Bearer ', '');
   let payload;
   try {
     payload = jwt.verify(token, JWT_SECRET);
   } catch (err) {
-    return res.status(ERROR_MESSAGES.UNAUTHORIZED.status).send(ERROR_MESSAGES.UNAUTHORIZED.message);
+    return res.status(ERROR_MESSAGES.UNAUTHORIZED.status).send({ message: ERROR_MESSAGES.UNAUTHORIZED.message });
   }
   req.user = payload;
   next();
